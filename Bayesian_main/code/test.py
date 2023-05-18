eca4eb8a1e17d4ebd208f50530bc527235f1eae2
Leonardo-1107 <c.j.steve@sjtu.edu.cn>
2023-05-18 14:12:45 +0800

    5.18 update

--- Bayesian_main/code/test.py
+++ Bayesian_main/code/test.py
@@ -6,26 +6,29 @@ import os
 
 
 if __name__=="__main__":
-    path_list = ['bm_lis_go_sesrp', 'nefb_fb_hlc_cir', 'tok_lad_scsr_ahc', 'Washington','North_Idaho']
+    path_list = ['bm_lis_go_sesrp', 'nefb_fb_hlc_cir', 'tok_lad_scsr_ahc', 'North_Idaho', 'NovaScotia2', 'Washington']
     for name in path_list:
-        path = f'Bayesian_main/data/NovaScotia2.pkl'
+        # name = 'NovaScotia2'
+        path = f'Bayesian_main/data/{name}.pkl'
 
-    # Automatically decide an algorithm
-    # algo_list = [rfcAlgo, svmAlgo, logiAlgo]
-    # method = Method_select(algo_list)
-    # score = method.select(data_path=path, task=Model)
-    # algo = algo_list[score.index(max(score))]
-    # print("Use" + str(algo)) 
+        #Automatically decide an algorithm
+        # algo_list = [rfcAlgo, svmAlgo, logiAlgo]
+        # method = Method_select(algo_list)
+        # score = method.select(data_path=path, task=Model)
+        # algo = algo_list[score.index(max(score))]
+        # print("Use" + str(algo)) 
 
-    # Bayesian optimization process
+        # Bayesian optimization process
         bo = Bayesian_optimization(
             data_path=path, 
-            algorithm=logiAlgo, 
-            mode='k',
+            algorithm=rfcAlgo, 
+            mode='random',
+            metrics=['f1','pre','auc'],
             default_params= True
             )
         
-        x_best = bo.optimize(20)
+        x_best = bo.optimize(100)
         
         # for rfcAlgo estimation
-        print(f'Best   n_estimators: {x_best[0]}, max_depth: {x_best[1]}, criterion: {x_best[2]}')
+        # print(f'Best   n_estimators: {x_best[0]}, max_depth: {x_best[1]}, criterion: {x_best[2]}')
+        
